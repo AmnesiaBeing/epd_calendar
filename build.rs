7aa1fb12f3c1c0b1c063d3ab40f3234086cd78f2
AmnesiaBeing <727184139@qq.com>
2025-12-15 07:08:46 +0800

    临时提交

---
+++ build.rs
@@ -0,0 +1,70 @@
+//! 嵌入式资源构建脚本 - 主入口
+
+// mod builder;
+
+use anyhow::{Ok, Result};
+
+fn main() -> Result<()> {
+    // builder::run()?;
+
+    #[cfg(feature = "embedded_esp")]
+    {
+        linker_be_nice();
+        println!("cargo:rustc-link-arg=-Tdefmt.x");
+        println!("cargo:rustc-link-arg=-Tlinkall.x");
+    }
+
+    Ok(())
+}
+
+#[cfg(feature = "embedded_esp")]
+fn linker_be_nice() {
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() > 1 {
+        let kind = &args[1];
+        let what = &args[2];
+
+        match kind.as_str() {
+            "undefined-symbol" => match what.as_str() {
+                "_defmt_timestamp" => {
+                    eprintln!();
+                    eprintln!(
+                        "💡 `defmt` not found - make sure `defmt.x` is added as a linker script and you have included `use defmt_rtt as _;`"
+                    );
+                    eprintln!();
+                }
+                "_stack_start" => {
+                    eprintln!();
+                    eprintln!("💡 Is the linker script `linkall.x` missing?");
+                    eprintln!();
+                }
+                "esp_rtos_initialized" | "esp_rtos_yield_task" | "esp_rtos_task_create" => {
+                    eprintln!();
+                    eprintln!(
+                        "💡 `esp-radio` has no scheduler enabled. Make sure you have initialized `esp-rtos` or provided an external scheduler."
+                    );
+                    eprintln!();
+                }
+                "embedded_test_linker_file_not_added_to_rustflags" => {
+                    eprintln!();
+                    eprintln!(
+                        "💡 `embedded-test` not found - make sure `embedded-test.x` is added as a linker script for tests"
+                    );
+                    eprintln!();
+                }
+                _ => (),
+            },
+            // we don't have anything helpful for "missing-lib" yet
+            _ => {
+                std::process::exit(1);
+            }
+        }
+
+        std::process::exit(0);
+    }
+
+    println!(
+        "cargo:rustc-link-arg=--error-handling-script={}",
+        std::env::current_exe().unwrap().display()
+    );
+}
